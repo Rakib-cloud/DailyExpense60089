1685074e81a8b7186989168f8b945f57a1617652
Md .Rakibul Islam <57381896+Rakib-cloud@users.noreply.github.com>
2023-06-14 16:08:48 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const router = require("./routes/transactions");
+// const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+
+// const PORT = 4000
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+
+
+app.use("/api/v1", router)
+
+const server = () => {
+    db()
+    // app.listen(PORT, "127.0.0.1", () => {
+    //     console.log('listening to port:', PORT)
+    // })
+    app.listen(5000, () => {
+        console.log("Running on port 5000.");
+      });
+}
+
+server()
